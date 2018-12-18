1f812e0223d79b734399d98cdfe30471422d1cfd
Aric <arickuter99@gmail.com>
2018-12-18 12:24:47 +0200

    feat: Add extra categories and items to db

--- lotsofcat.py
+++ lotsofcat.py
@@ -30,4 +30,110 @@ items1 = Items(title="Soccer ball", description="Limited edition ball", categori
 session.add(items1)
 session.commit()
 
+################
+
+# Basketball category
+category1 = Categories(name="Basketball")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Basket ball", description="Limited edition ball", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Baseball category
+category1 = Categories(name="Baseball")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Baseball bat", description="Limited edition bat", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Frisbee category
+category1 = Categories(name="Frisbee")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Frisbee", description="Limited edition frisbee", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Snowboarding category
+category1 = Categories(name="Snowboarding")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Snowboard", description="Limited edition snowboard", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Rock Climbing category
+category1 = Categories(name="Rock Climbing")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Rope", description="200ft of rope", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Foosball category
+category1 = Categories(name="Foosball")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Table", description="Limited edition table", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Skating category
+category1 = Categories(name="Skating")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Skateboard", description="Limited edition skateboard", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
+# Hockey category
+category1 = Categories(name="Hockey")
+
+session.add(category1)
+session.commit()
+
+items1 = Items(title="Hockey stick", description="Limited edition stick", categories=category1)
+
+session.add(items1)
+session.commit()
+
+################
+
 print "added menu items!"
