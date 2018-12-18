8ee3a6c24f13e3f30e9abec283e24433cf5c5121
Aric <arickuter99@gmail.com>
2018-12-18 12:16:11 +0200

    feat: Create a database

---
+++ database_setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Categories(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+class Items(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    cat_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'cat_id': self.cat_id,
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
