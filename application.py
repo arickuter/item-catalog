a5c93098ca7c24af92c8fb1168c86d8f003cbde5
Aric <arickuter99@gmail.com>
2019-01-10 21:58:26 +0200

    feat: Enable the user to logout
    feat: Protect certain pages from being viewed unless logged in

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = "Restaurant Menu Application"
+APPLICATION_NAME = "Aric's Catalog App"
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db', pool_pre_ping=True)
@@ -29,6 +29,8 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Create anti-forgery state token
+
+
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
@@ -36,6 +38,7 @@ def showLogin():
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -105,9 +108,8 @@ def gconnect():
 
     data = answer.json()
 
-    login_session['username'] = data['name']
+    login_session['username'] = data['email']
     login_session['picture'] = data['picture']
-    login_session['email'] = data['email']
 
     output = ''
     output += '<h1>Welcome, '
@@ -120,31 +122,75 @@ def gconnect():
     print "done!"
     return output
 
+
 @app.route('/')
 def catalogHome():
     session = DBSession()
     category = session.query(Categories).all()
     item = session.query(Items).all()
-    return render_template('home.html', category=category, item=item)
+    if 'username' not in login_session:
+        loggedIn = False
+        return render_template('home.html', category=category, item=item, loggedIn=loggedIn)
+    else:
+        loggedIn = True
+        userUsername = login_session['username']
+        return render_template('home.html', category=category, item=item, loggedIn=loggedIn, userUsername=userUsername)
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps(
+            'Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['picture']
+        flash('You were successfully logged out')
+        return redirect(url_for('catalogHome'))
+    else:
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 @app.route('/catalog/<category_name>/items')
 def catalogDisplay(category_name):
-    session = DBSession()
-    categoryDisplay = session.query(
-        Categories).filter_by(name=category_name).one()
-    category = session.query(Categories).all()
-    item = session.query(Items).all()
-    return render_template('category.html', category=category, item=item, categoryDisplay=categoryDisplay)
+    if 'username' not in login_session:
+        return redirect('/login')
+    else:
+        session = DBSession()
+        categoryDisplay = session.query(
+            Categories).filter_by(name=category_name).one()
+        category = session.query(Categories).all()
+        item = session.query(Items).all()
+        return render_template('category.html', category=category, item=item, categoryDisplay=categoryDisplay)
 
 
 @app.route('/catalog/<category_name>/<item_name>')
 def descriptionDisplay(category_name, item_name):
-    session = DBSession()
-    categoryDisplay = session.query(
-        Categories).filter_by(name=category_name).one()
-    item = session.query(Items).filter_by(title=item_name).one()
-    return render_template('description.html', categoryDisplay=categoryDisplay, item=item)
+    if 'username' not in login_session:
+        return redirect('/login')
+    else:
+        session = DBSession()
+        categoryDisplay = session.query(
+            Categories).filter_by(name=category_name).one()
+        item = session.query(Items).filter_by(title=item_name).one()
+        return render_template('description.html', categoryDisplay=categoryDisplay, item=item)
 
 
 if __name__ == '__main__':
