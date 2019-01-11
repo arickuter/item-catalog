7aa073cdd8ddfd3db9fef746869759f5d89936a5
Aric <arickuter99@gmail.com>
2019-01-11 20:58:29 +0200

    fix: Not logging out fix

--- application.py
+++ application.py
@@ -153,21 +153,13 @@ def gdisconnect():
 
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
-    result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
-    if result['status'] == '200':
-        del login_session['access_token']
-        del login_session['gplus_id']
-        del login_session['username']
-        del login_session['picture']
-        flash('You were successfully logged out')
-        return redirect(url_for('catalogHome'))
-    else:
-        response = make_response(json.dumps(
-            'Failed to revoke token for given user.', 400))
-        response.headers['Content-Type'] = 'application/json'
-        return response
+
+    del login_session['access_token']
+    del login_session['gplus_id']
+    del login_session['username']
+    del login_session['picture']
+    flash('You were successfully logged out')
+    return redirect(url_for('catalogHome'))
 
 
 @app.route('/catalog/<category_name>/items')
